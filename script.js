b98353ad8d9625d8035918e8bfd987dbe397b7fa
jeremyainsworth0791 <54958736+jeremyainsworth0791@users.noreply.github.com>
2021-03-20 15:56:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ window.addEventListener("load",function(){
     todayWeather = document.getElementsByClassName("");
     nameSearch = document.getElementById("city-search")
 
-    //Instatiate table data
+    //Variables 
 
     var cityName = document.getElementById("city-name");
     var cityTemp = document.getElementById("tempInput");
     var cityWind = document.getElementById("windInput");
     var cityHum  = document.getElementById("humidityInput");
     var cityUv   = document.getElementById("uvIndexInput");
-    var cityImg  = document.getElementById("weatherIcon");
-
+    var savedCity = document.getElementById("stored-searches");
+    var dayDate  = document.getElementById("date-today")
+    var fiveDay = document.getElementById("future-forecast");
+    var currentDate = "("+moment().format('l')+")";
+    var cityButtons = document.getElementsByClassName("stored-buttons");
+   
+   
     // api.openweathermap.org/data/2.5/weather?q={city name}&appid=820da3b7a8a561cc5f603199f9d93f15
+   
     function searchWeather(searchedName) {
         var fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchedName}&units=imperial&appid=820da3b7a8a561cc5f603199f9d93f15`
 
@@ -24,10 +30,11 @@ window.addEventListener("load",function(){
             console.log(data)
             //parse data 
                 // define each element
+                dayDate.innerHTML = currentDate;
             
             postWeather(data);
             var coordinate = data.coord;
-            getUVIndex(coordinate);
+            getUVIndex(coordinate); 
         });
         
         // append all the elements together
@@ -38,7 +45,8 @@ window.addEventListener("load",function(){
     }
     //Function that posts todays weather 
     function postWeather(dataWeather){
-        console.log(dataWeather)
+         console.log(dataWeather)
+         
          cityName.innerHTML= dataWeather.name;
         //  cityImg.src = dataWeather.weather[0].icon;
          cityTemp.innerHTML= dataWeather.main.temp;
@@ -56,24 +64,161 @@ window.addEventListener("load",function(){
         })
         .then(function(data){
             cityUv.innerHTML = data.current.uvi;
+            uvStyles(data.current.uvi);
         })
     }
+    //function for displaying teh 5-day forecast
+    function getForecast(searchedName) {
+        fetchUrl= `api.openweathermap.org/data/2.5/forecast/daily?q=${searchedName}&cnt=5&units=imperial&appid=820da3b7a8a561cc5f603199f9d93f15`
+        fetch(fetchUrl)
+        .then(function (response){
+            return response.json();
+        })
+         .then(function(data){
+             fiveDay.innerHTML = "";
+             for(i=0; i<5; i++){
+                              
+                var dateCard = document.createElement('h4');
 
-    function getForecast() {
-         
+                var cardTemp = document.createElement('p');
+                var cardHumid = document.createElement('p');
+
+                dateCard.innerHTML = moment().add((1+i), 'days').format('L');
+                cardTemp.innerHTML = data.list[i].main.temp;
+                cardHumid.innerHTML = data.list[i].main.humidity+" %";
+
+                // dayCard.appendChild(dateCard);
+                // dayCard.appendChild(dayTemp);
+                // dayCard.appendChild(dayHumid);
+                
+             }
+         })
 
     }
+
+    function uvStyles(uvIndex){
+
+    
+        if(uvIndex > 0 && (uvIndex < 3)) {
+    
+            cityUv.className = "low";
+                
+        }
+    
+        if(uvIndex > 3 && (uvIndex < 6)){
+    
+            cityUv.className = "medium";
+                
+        }
+    
+        if(uvIndex > 6 && (uvIndex < 8)){
+    
+            cityUv.className = "high";
+            
+         }
+    
+        if(uvIndex > 8 && (uvIndex < 11)){
+    
+            cityUv.className = "very-high";
+                
+        }
+        if(uvIndex > 11 && (uvIndex < 3)){
+    
+            cityUv.className = "extreme";
+    
+        }
+    }
+
+    function storeButtons(cityToStore){
+
+        var savedCities = localStorage.getItem("city list");
+    
+        if(savedCities){
+    
+            savedCities = JSON.parse(savedCities);
+            savedCities.push(cityToStore);
+            localStorage.setItem("city list", JSON.stringify(savedCities));
+    
+        }else{
+    
+            savedCities = []
+            savedCities.push(cityToStore);
+            localStorage.setItem("city list", JSON.stringify(savedCities));
+    
+        }
+    }
+    
+    function printStoreButtons(){
+
+        var savedCities = localStorage.getItem("city list");
+    
+        if(savedCities){
+    
+            savedCities = JSON.parse(savedCities);
+    
+            for(i=0; i<savedCities.length; i++){
+    
+                var newButton = document.createElement('button');
+                newButton.innerHTML = savedCities[i];
+                newButton.setAttribute("class", "stored-buttons");
+    
+                newButton.addEventListener("click", function(e){
+    
+                    nameSearch = this.innerHTML;
+                    searchWeather(nameSearch);
+                    getUVIndex(coordinate);
+                    // futureForcasts(searchedCity);
+                    storeButtons(nameSearch);
+    
+                })
+    
+                savedCity.appendChild(newButton);
+    
+            }
+    
+        }
+    }
+
+    function updateSavedList(){
+
+        var savedCities = localStorage.getItem("city list");
+    
+        if(savedCities){
+    
+            savedCities = JSON.parse(savedCity);
+    
+            var newButton = document.createElement('button');
+            newButton.innerHTML = searchInput.value;
+            newButton.setAttribute("class", "stored-buttons");
+    
+            newButton.addEventListener("click", function(e){
+    
+                nameSearch = this.innerHTML;
+                searchWeather(nameSearch);
+                // futureForcasts(searchedCity);
+                storeButtons(nameSearch);
+    
+            })
+    
+            savedCity.appendChild(newButton);
+    
+        }
+    }
+
     searchButton.addEventListener("click", function(){
-        console.log("works")
+
        var nameSearch = document.getElementById("city-search").value;
-    //    var searchedName=nameSearch.value;
+    
        console.log(nameSearch)
        if(nameSearch){
-           searchWeather(nameSearch)
-           // clear the input field
-           // deal with adding it to the list and to the history
+           searchWeather(nameSearch);
+           getForecast(nameSearch);
+           storeButtons(nameSearch);
+           updateSavedList();
+           
        } 
+       
     })
-
+    printStoreButtons();
 })
 
